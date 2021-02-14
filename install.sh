d4d780dfef124a7aaa4aebdcc836477638619c27
Harsh Singh <harshsngh07@gmail.com>
2021-02-14 21:47:45 +0530

    added script files

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #clone the repo
-git clone 
+git clone https://gitlab.crio.do/COHORT_ME_BUILDOUT_XMEME_ENROLL_1612436694845/harshsngh07-me_buildout_xmeme.git 
 #install nodejs
 
+
+cd client
 #install all the dependencies
 npm install
 
--- server_run.sh
+++ server_run.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+
+git clone https://gitlab.crio.do/COHORT_ME_BUILDOUT_XMEME_ENROLL_1612436694845/harshsngh07-me_buildout_xmeme.git 
+
 #cd in to server
 cd server
 #install all the dependencies of server
